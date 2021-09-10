3b8d737804f8b70f09f12ca283067c9079b24d66
thynkon <thynkon@protonmail.com>
2021-09-10 10:58:39 +0200

    Rename questionnaire table to 'quiz'

--- database/select.sql
+++ database/select.sql
@@ -1,28 +1,28 @@
 use looper;
 
-# Get list of answering questionnaires that are public (/exercises/answering)
-SELECT questionnaire.title
-FROM questionnaire
-INNER JOIN questionnaire_state ON questionnaire_state.id = questionnaire.questionnaire_state_id
-WHERE questionnaire_state.label = "Answering"
-AND questionnaire.is_public = 1;
+# Get list of answering quizs that are public (/exercises/answering)
+SELECT quiz.title
+FROM quiz
+INNER JOIN quiz_state ON quiz_state.id = quiz.quiz_state_id
+WHERE quiz_state.label = "Answering"
+AND quiz.is_public = 1;
 
-# Fetch building questionnaires (is_public = 0) (usefull to show button 'be ready for answers)
-SELECT questionnaire.title, questionnaire_state.label AS state, COUNT(*) AS questions
-FROM questionnaire
-INNER JOIN questionnaire_state ON questionnaire_state.id = questionnaire.questionnaire_state_id
-INNER JOIN question ON question.questionnaire_id = questionnaire.id
-WHERE questionnaire.is_public = 1;
+# Fetch building quizs (is_public = 0) (usefull to show button 'be ready for answers)
+SELECT quiz.title, quiz_state.label AS state, COUNT(*) AS questions
+FROM quiz
+INNER JOIN quiz_state ON quiz_state.id = quiz.quiz_state_id
+INNER JOIN question ON question.quiz_id = quiz.id
+WHERE quiz.is_public = 1;
 
-# Show statistics for a questionnaire
+# Show statistics for a quiz
 SELECT answer.date, answer.value
 FROM answer
 INNER JOIN question ON question.id = answer.question_id
-INNER JOIN questionnaire ON questionnaire.id = question.questionnaire_id
-WHERE questionnaire.id = 2;
+INNER JOIN quiz ON quiz.id = question.quiz_id
+WHERE quiz.id = 2;
 
-# Fetch questionnaire's questions (TO BE FIXED!!!)
-SELECT id, question.label, question_type.label
+# Fetch quiz's questions (TO BE FIXED!!!)
+SELECT question.id, question.label, question_type.label
 FROM question
 INNER JOIN question_type ON question_type.id = question.question_type_id
-WHERE question.questionnaire.id = 1;
+WHERE question.quiz_id = 1;
