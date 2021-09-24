2d1db44064487ce76576a0c28081daeda5042dbc
thynkon <thynkon@protonmail.com>
2021-09-24 15:58:37 +0200

    Fix forms urls and add names to inputs

--- resources/views/question/add.php
+++ resources/views/question/add.php
@@ -1,15 +1,15 @@
 <div id="new_question_parent" class="pure-u-1-3 wrapper">
-    <form class="pure-form pure-form-stacked">
+    <form class="pure-form pure-form-stacked" action="/question/create" method="post">
         <fieldset>
             <legend>New Question</legend>
             <fieldset class="pure-group">
                 <label for="question_label">Label</label>
-                <input type="text" id="question_label" class="pure-u-1-1" required="required" placeholder="Label"/>
+                <input type="text" id="question_label" class="pure-u-1-1" name="question_label" required="required" placeholder="Label"/>
             </fieldset>
 
             <fieldset class="pure-group">
                 <label for="question_type">Value type</label>
-                <select id="question_type">
+                <select id="question_type" name="question_type">
                     <option selected="selected">Single line text</option>
                     <option>List of single lines</option>
                     <option>Multi-line text</option>
--- resources/views/quiz/add.php
+++ resources/views/quiz/add.php
@@ -1,9 +1,9 @@
 <div id="new_quiz_parent" class="pure-g">
-    <form class="pure-u-3-5 pure-form pure-form-stacked">
+    <form class="pure-u-3-5 pure-form pure-form-stacked" action="/quiz/store" method="post">
         <fieldset>
             <legend>Create a new quiz</legend>
             <label for="quiz_title">Title</label>
-            <input type="text" id="quiz_title" class="pure-u-1-1" required placeholder="Title"/>
+            <input type="text" id="quiz_title" class="pure-u-1-1" name="quiz_title" required placeholder="Title"/>
             <button type="submit" class="pure-u-2-5 pure-button pure-button-primary upper-case">Create</button>
         </fieldset>
     </form>
