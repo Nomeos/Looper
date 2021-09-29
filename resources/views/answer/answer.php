535cee9bcd50458f69e6e86646b6515d4a8d0c01
Nomeos <mathieu.rabot01@gmail.com>
2021-09-29 12:58:27 +0200

    add admin views and answering view

---
+++ resources/views/answer/answer.php
@@ -0,0 +1,8 @@
+<div id="quiz_buttons_parent" class="pure-g">
+    <a id="take_quiz" class="pure-u-1-5 pure-button pure-button-primary upper-case" href="/quizzes/answering">Take a
+        quiz</a>
+    <a id="take_quiz" class="pure-u-1-5 pure-button pure-button-primary upper-case" href="/quizzes/answering">Take a
+        quiz</a>
+    <a id="take_quiz" class="pure-u-1-5 pure-button pure-button-primary upper-case" href="/quizzes/answering">Take a
+        quiz</a>
+</div>
