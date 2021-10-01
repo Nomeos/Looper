05e5d3ee1909c8e302008ca6691c207abfca0ff1
Nomeos <mathieu.rabot01@gmail.com>
2021-10-01 10:37:36 +0200

    add reply, admin and answer static views

--- resources/views/answer/answer.php
+++ resources/views/answer/answer.php
@@ -1,8 +1,13 @@
-<div id="quiz_buttons_parent" class="pure-g">
-    <a id="take_quiz" class="pure-u-1-5 pure-button pure-button-primary upper-case" href="/quizzes/answering">Take a
-        quiz</a>
-    <a id="take_quiz" class="pure-u-1-5 pure-button pure-button-primary upper-case" href="/quizzes/answering">Take a
-        quiz</a>
-    <a id="take_quiz" class="pure-u-1-5 pure-button pure-button-primary upper-case" href="/quizzes/answering">Take a
-        quiz</a>
+<div class="wrapper pure-g">
+    <div class="quiz pure-u-2-3">
+        <label for="take_quiz">Ex</label>
+        <a class=" pure-button pure-button-primary upper-case" href="/quizzes/answering">Take a
+            quiz</a>
+    </div>
+
+    <div class="quiz pure-u-2-3">
+        <label for="take_quiz">Ex</label>
+        <a class=" pure-button pure-button-primary upper-case" href="/quizzes/answering">Take a
+            quiz</a>
+    </div>
 </div>
