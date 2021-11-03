792cdc4214ed746d44732795957340b31ec817d1
thynkon <thynkon@protonmail.com>
2021-11-03 19:52:26 +0100

    Improve error check after calling js promise

--- public/assets/js/js.js
+++ public/assets/js/js.js
@@ -1,11 +1,29 @@
-
-const button = document.querySelector('#delete1');
-
-button.addEventListener('click', function (event) {
-    //tell the browser not to respond to the link click
-    event.preventDefault();
-
+function doRequest(button) {
     return fetch(button.href, {
         method: button.getAttribute('data-method'),
-    }).then(response => alert(response));
-});
+    }).then(response => response);
+}
+
+function main() {
+    const buttons = document.querySelectorAll('a[data-method]');
+
+    buttons.forEach(function (button) {
+        console.log(button)
+        button.addEventListener('click', function (event) {
+            // tell the browser not to respond to the link click
+            event.preventDefault();
+            doRequest(button)
+                .then(function (response) {
+                    if (response.status !== 200) {
+                        console.log("ERROR");
+                        // ADD ERROR MESSAGE TO HTML PAGE
+                    } else {
+                        window.location.replace('/quiz/admin');
+                    }
+                })
+                .catch(error => console.log("ERROR"));
+        });
+    });
+}
+
+main();
