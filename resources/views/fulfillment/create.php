1a13c104fa080853d7095c3da8847bc69dc93089
thynkon <thynkon@protonmail.com>
2021-11-16 18:16:42 +0100

    Display fulfillment input depending on the question's type

--- resources/views/fulfillment/create.php
+++ resources/views/fulfillment/create.php
@@ -1,16 +1,23 @@
 <div id="new_quiz_parent" class="pure-g">
-    <form class="pure-u-3-5 pure-form pure-form-stacked" action="/quiz/<?= $data["body"]["quiz"]->id?>/fullfilment" method="post">
+    <form class="pure-u-3-5 pure-form pure-form-stacked" action="/quiz/<?= $data["body"]["quiz"]->id ?>/fullfilment"
+          method="post">
         <fieldset>
             <legend>Your Take</legend>
             <div>If you'd like to come back later to finish, simply submit it with blanks.</div>
             <br>
             <?php foreach ($questions as $question): ?>
                 <label for="quiz_title"><?= $question->label ?></label>
-                <input name="<?= $question->id ?>" type="text" id="quiz_title" class="pure-u-1-1" required
-                       placeholder="Your answer.."/>
-            <br>
+                <?php if ($question->type()->label === "Single line text"): ?>
+                    <input name="<?= $question->id ?>" type="text" class="pure-u-1-1"
+                           placeholder="Your answer.."/>
+                    <br>
+                <?php else: ?>
+                    <textarea name="<?= $question->id ?>" class="pure-u-1-1"
+                              placeholder="Your answer.."></textarea>
+                    <br>
+                <?php endif; ?>
             <?php endforeach; ?>
-            <button type="submit" class="pure-u-2-5 pure-button pure-button-primary">SAVE</button>
+            <button type="submit" class="pure-u-1-1 pure-button pure-button-primary">SAVE</button>
         </fieldset>
     </form>
 </div>
