9c8f3b20d6a3b48ddb69fe5b01e5b120431dd905
thynkon <thynkon@protonmail.com>
2021-11-17 08:23:20 +0100

    Redirect user to /quiz/admin after publishing quiz

--- public/assets/js/js.js
+++ public/assets/js/js.js
@@ -6,20 +6,25 @@ function doRequest(button) {
 
 function main() {
     const buttons = document.querySelectorAll('a[data-method]');
-    let current_location = window.location.href;
+    let url = window.location.href;
+    let publish_quiz_button_id = "publish_quiz";
 
     buttons.forEach(function (button) {
-        console.log(button)
         button.addEventListener('click', function (event) {
             // tell the browser not to respond to the link click
             event.preventDefault();
+
+            if (button.id === publish_quiz_button_id) {
+                url = "/quiz/admin";
+            }
+
             doRequest(button)
                 .then(function (response) {
                     if (response.status !== 200) {
                         console.log("ERROR");
                         // ADD ERROR MESSAGE TO HTML PAGE
                     } else {
-                        window.location.replace(current_location);
+                        window.location.replace(url);
                     }
                 })
                 .catch(error => console.log("ERROR"));
